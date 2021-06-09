f56dc8828555a4290c359d6141bb4b3c763e6579
Cristina Fernandez baldovinos <crisfern@c2r6s3.42madrid.com>
2021-06-09 16:59:47 +0200

    Updated

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,7 @@
 #include "get_next_line.h"
 
-char	*read_text(int fd, char *str)
+char	*read_text(int fd, char *str, char *buffer)
 {
-	char	buffer[BUFFER_SIZE + 1];
 	char	*aux;
 	int		n_bytes;
 
@@ -13,14 +12,14 @@ char	*read_text(int fd, char *str)
 		{
 			buffer[n_bytes] = 0;
 			if (!str)
-				str = ft_strdup(buffer);
+				str = ft_strdup(buffer); 
 			else
 			{
 				aux = ft_strjoin(str, buffer);
 				free(str);
 				str = aux;
 			}
-			if (n_bytes != BUFFER_SIZE)
+			if (n_bytes != BUFFER_SIZE);
 				break ;
 		}
 		else
@@ -32,17 +31,23 @@ char	*read_text(int fd, char *str)
 char	*get_line(char *str, char **line)
 {
 	char	*pos;
+	char	*aux;
 
 	if (str)
 	{
 		pos = ft_strchr(str, '\n');
 		if (pos)
-			line[0] = ft_substr(str, 0, pos - str);
+		{
+			aux = ft_substr(str, 0, pos - str);
+			pos = ft_strchr(aux, '\n');
+		}
 		else
 		{
-			pos = ft_strchr(str, '\0');
-			line[0] = ft_substr(str, 0, pos - str);
+			aux= ft_strdup(str);
+			pos = ft_strchr(aux, '\0');
 		}
+		line[0] = aux;
+		free(str);
 		return (pos);
 	}
 	return (0);
@@ -51,22 +56,26 @@ char	*get_line(char *str, char **line)
 int	get_next_line(int fd, char **line)
 {
 	static char	*str;
+	char		*buffer;
+	int			a;
 
-	if (str && !*str)
-		return (0);
-	else
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (buffer)
 	{
 		if (!str)
-			str = read_text(fd, str);
-		else
-			str++;
+			str = read_text(fd, str, buffer);
 		if (str)
 		{
 			str = get_line(str, line);
-			return (1);
+			if (!*str)
+				a = 0;
+			else
+				a = 1;
 		}
+		free(buffer);
 	}
-	return (-1);
+	a = -1;
+	return (a);
 }
 
 int main()
@@ -75,7 +84,19 @@ int main()
 	int		a;
 	char	**line;
 
-	fd = open("prueba.txt", O_RDONLY);
+	fd = open("normal.txt", O_RDONLY);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	/*a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
 	a = get_next_line(fd, line);
 	printf("|%s| %d\n", line[0], a);
 	a = get_next_line(fd, line);
@@ -86,5 +107,277 @@ int main()
 	printf("|%s| %d\n", line[0], a);
 	a = get_next_line(fd, line);
 	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);
+	a = get_next_line(fd, line);
+	printf("|%s| %d\n", line[0], a);*/
 	close(fd);
 }
